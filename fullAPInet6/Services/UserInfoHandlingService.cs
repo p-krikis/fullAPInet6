54b1efe83357a9891c84178fa59ec30fee2a266e
p-krikis <p.krikis@prisma2015.onmicrosoft.com>
2023-06-06 16:55:29 +0300

    added preliminary support for updating user info

--- fullAPInet6/Services/UserInfoHandlingService.cs
+++ fullAPInet6/Services/UserInfoHandlingService.cs
@@ -93,6 +93,34 @@ namespace fullAPInet6.Services
                 }
             }
         }
+        //public async Task<string> UpdateUserInfo (string targetUserId)
+        //{
+        //    using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+        //    {
+        //        connection.Open();
+        //        using (var command = new SqlCommand("UPDATE email, username, displayName, role FROM [dbo].[userInfoFull] WHERE userid = @userid", connection))
+        //        {
+        //            command.Parameters.AddWithValue("@userid", targetUserId);
+
+        //            using (var reader = await command.ExecuteReaderAsync())
+        //            {
+        //                List<UserInfoResponse> responseList = new List<UserInfoResponse>();
+
+        //                while (await reader.ReadAsync())
+        //                {
+        //                    responseList.Add(new UserInfoResponse()
+        //                    {
+        //                        email = reader.GetString(0),
+        //                        username = reader.GetString(1),
+        //                        displayName = reader.GetString(2),
+        //                        role = reader.GetString(3)
+        //                    });
+        //                }
+        //                return responseList;
+        //            }
+        //        }
+        //    }
+        //}
         public async Task DeleteUserByListId(string userid)
         {
             using (var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
